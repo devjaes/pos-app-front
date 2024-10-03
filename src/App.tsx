e0cd37d52351baff6179365da9a6e6c233d20ee0
devjaes <merajair17@gmail.com>
2024-10-03 12:52:44 -0500

    chore: init repository, lint rules, primereact

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { PrimeReactProvider } from 'primereact/api';
+
+function App() {
+  return (
+    <PrimeReactProvider>
+      <h1>App</h1>
+    </PrimeReactProvider>
+  );
+}
+
+export default App;
